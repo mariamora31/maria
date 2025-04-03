9c58529e154f15241ea1067dc36c2c74ed91dc2d
mariamora31 <142507796+mariamora31@users.noreply.github.com>
2025-04-03 20:59:27 -0300

    Add files via upload

---
+++ maria-main/maria-main/maria-main/mora/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acesibilidade')
+    const opcoesDeAcessibilidade=document.getElementById
+
+    botaoDeAcessibilidade.addEventListener('click', function()[
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')
+        const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')=== 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded',!botaoSelecionado)
+
+])
+
+const aumentaFonteBotao = document.getElementById('aumentar-font');
+const diminuiFonte = doumente.getElementById('diminuir-fonte');
+
+const Contraste = document.getElementById('alterna-contraste');
+}) 
+
--- maria-main/maria-main/mora/JS scripy.js
+++ maria-main/maria-main/mora/JS scripy.js
@@ -1,28 +1,4 @@
 document.addEventListener('DOMContentLoaded',function(){
 
-    const botaoDeAcessibilidade=document.getElementById('botao-acesibilidade')
-    const opcoesDeAcessibilidade=document.getElementById
-
-    botaoDeAcessibilidade.addEventListener('click', function()[
-        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
-
-        const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')
-
-
-
-
-
-
-
-
-
-    ])
-
-
-
-
-
-
+    const botaoDeAcessibilidade=document.getElementById('botao-acesibilidade');
 }) 
-
