150cf28902aecd22bc9647ecc9b35ee6a51c2070
mariamora31 <142507796+mariamora31@users.noreply.github.com>
2025-04-02 21:01:34 -0300

    Add files via upload

---
+++ maria-main/maria-main/mora/JS scripy.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+    const botaoDeAcessibilidade=document.getElementById('botao-acesibilidade')
+    const opcoesDeAcessibilidade=document.getElementById
+
+    botaoDeAcessibilidade.addEventListener('click', function()[
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+
+        const botaoSelecionado=botaoDeAcessibilidade.getAttribute('aria-expanded')
+
+
+
+
+
+
+
+
+
+    ])
+
+
+
+
+
+
+}) 
+
